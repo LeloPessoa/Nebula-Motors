c4dd8ed72d28d7a3b31d2cb9bd21d426f6242719
Aurelio <aurelioipssss@gmail.com>
2024-03-06 16:16:33 -0300

    .

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const verMaisBtn = document.getElementById("verMaisBtn");
     const novosVeiculos = document.querySelectorAll(".new-vehicle");
-
     verMaisBtn.addEventListener("click", function() {
-        // Alterna a classe "hidden" dos novos veículos para mostrar ou ocultar
         novosVeiculos.forEach(function(veiculo) {
             veiculo.classList.toggle("hidden");
         });
-
-        // Altera o texto do botão para "Ver Mais" ou "Ver Menos" com base na visibilidade dos novos veículos
         if (novosVeiculos[0].classList.contains("hidden")) {
             verMaisBtn.textContent = "Ver Mais";
         } else {
